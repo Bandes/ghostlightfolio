51ded1710f77a8b0f756545064aed8d0cfbb3549
Charles Bandes <charley@charlesbandes.com>
2021-08-01 21:28:56 -0400

    Adds hotwire and alpinejs to projct

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,12 +4,15 @@
 // that code so it'll be compiled.
 
 import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
+import "@hotwired/turbo-rails"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
 Rails.start()
-Turbolinks.start()
 ActiveStorage.start()
 import "stylesheets/application"
 import "@fortawesome/fontawesome-free/css/all"
+import 'alpine-turbo-drive-adapter'
+import 'alpinejs'
+
+import "controllers"
