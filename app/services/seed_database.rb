c9516b52c5f7a9066efb71fe3ef4be1ada79a713
Charles Bandes <charley@charlesbandes.com>
2021-08-08 17:26:08 -0400

    Adds seed data and seeding service class
    This adds two json files containing show/author/role data, and a seeding service to seed this data. I haven't really figured out how to test the seding service yet so I haven't included a test in this commit, but it will be coming soon because I am really trying to keep coverage at 100% if at all possible.

---
+++ app/services/seed_database.rb
@@ -0,0 +1,36 @@
+class SeedDatabase
+
+  def self.execute
+    seed_authors
+    seed_roles
+  end
+
+  private
+
+  def self.seed_authors
+    file = File.open('app/data/shows_with_authors.json')
+    parsed_data = JSON.parse(file.read)
+    parsed_data.each do |datum|
+      author = Author.find_or_initialize_by(author_code: datum['author_code'])
+      author.update(
+        first_name: datum['first_name'],
+        last_name: datum['last_name'],
+      )
+
+      show = Show.find_or_initialize_by(show_code: datum['show_code'], name: datum['show_name'])
+      show.update(author: author)
+    end
+
+  end
+
+  def self.seed_roles
+    file = File.open('app/data/shows_with_roles.json')
+    parsed_data = JSON.parse(file.read)
+
+    parsed_data.each do |datum|
+      show = Show.find_or_create_by(show_code: datum['show_code'], name: datum['show_name'])
+      role = Role.find_or_create_by(show: show, name: datum['role_name'])
+    end
+  end
+
+end
