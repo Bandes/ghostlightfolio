32835b088e5c5510896d29b9662ec0cc2bdfa7dc
Charles Bandes <charley@charlesbandes.com>
2021-08-16 16:29:32 -0400

    Set up mailgun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,6 +16,16 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+  config.action_mailer.smtp_settings = {
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'ghostlightfolio.heroku.com',
+    :authentication => :plain,
+  }
+  config.action_mailer.delivery_method = :smtp
+
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
   # config.require_master_key = true
