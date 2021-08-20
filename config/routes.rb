a37b31bc75c4b2d0deede8973209f9b33c9cc193
Charles Bandes <charley@charlesbandes.com>
2021-08-20 13:44:17 -0400

    Adds create and destroy actions for roles
    This adds the ability to add and destroy roles for users with appropriate access levels. The test is not always passing and I'm not sure why so I am still working on that.

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   resources :people, only: %w[index new create update destroy edit]
   resources :locations, only: %w[index new create update destroy edit]
   resources :shows, only: %w[index new create update destroy edit show] do
-    resources :roles, only: %w[edit update]
+    resources :roles, only: %w[edit update new create destroy]
   end
   get 'shows/new_from_author/:author_id', to: 'shows#new_from_author'
 
